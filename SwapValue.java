77c647b2faae3581556b150fea9fda1bc4de0060
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-08-10 15:32:38 +0530

    Changes to be committed: 	modified:   SwapValue.class 	modified:   SwapValue.java

--- SwapValue.java
+++ SwapValue.java
@@ -1,5 +1,17 @@
 public class SwapValue {
     public static void main(String[] args) {
         System.out.println("Swapping 2 Numbers");
+        int a = 5;
+        int temp;
+        int b = 9;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        
+
     }
 }
