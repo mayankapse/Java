7b98834a3a2bf94c921a3e980bdead3e32b26335
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-08-10 15:27:01 +0530

    Changes to be committed: 	new file:   HelloWorld.class 	new file:   HelloWorld.java 	new file:   MathsOperation.class 	new file:   MathsOperation.java 	new file:   SwapValue.class 	new file:   SwapValue.java

---
+++ MathsOperation.java
@@ -0,0 +1,19 @@
+class MathsOperation
+{
+public static void main (String args[])
+{
+int a = 6;
+int b = 8;
+int sum = a + b; 
+int diff = a - b;
+int multi = a * b;
+int div = a / b;
+int rem = a % b;
+System.out.println("Operations on Numeric Characters");
+System.out.println("Sum = " + sum);
+System.out.println("Difference = " + diff);
+System.out.println("Multiplication = " + multi);
+System.out.println("Division with decimals = " + div);
+System.out.println("Reminder = " + rem);
+}
+}
---
+++ SwapValue.java
@@ -0,0 +1,5 @@
+public class SwapValue {
+    public static void main(String[] args) {
+        System.out.println("Swapping 2 Numbers");
+    }
+}
